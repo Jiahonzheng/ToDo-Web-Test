028cca063232788527475568afd1a96e1ecc477e
Jiahonzheng <jiahonzheng@gmail.com>
2017-09-11 12:52:37 +0800

    Test

--- src/modules/apiClient.js
+++ src/modules/apiClient.js
@@ -1,29 +1,11 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import axios from 'axios'
-import config from './config'
 
 var apiClient = axios.create({
-  baseUrl: config.apiUrl,
-  timeout: 1000,
-  withCredentials: true
+  timeout: 5000,
+  withCredentials: true,
+  headers: {'Content-Type': 'application/json;charset=UTF-8'}
 })
 
-apiClient.interceptors.request.use(function (request) {
-  return request
-}, function (error) {
-  return Promise.reject(error)
-})
-
-apiClient.interceptors.response.use(function (response) {
-  const request = response.config
-  console.log('>>>', request.method.toUpperCase(), request.url, request.params, '\n  ', response.status, response.data)
-  return response
-}, function (error) {
-  let {response, config: request} = error
-  if (request) {
-    console.log('>>>', request.method.toUpperCase(), request.url, request.params, '\n  ',response.status, response.data)
-  }
-  return Promise.reject(error)
-})
-
-Vue.prototype.$apiClient = apiClient
+// Vue.prototype.$http = apiClient
+export default apiClient

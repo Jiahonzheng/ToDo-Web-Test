5ceab28ba7f4fbc9799f2f128ef69f20ed9053c5
Jiahonzheng <jiahonzheng@gmail.com>
2017-09-11 20:53:21 +0800

    完善axios模块

--- src/modules/apiClient.js
+++ src/modules/apiClient.js
@@ -1,11 +1,12 @@
 // import Vue from 'vue'
 import axios from 'axios'
+import config from './config'
 
 var apiClient = axios.create({
   timeout: 5000,
   withCredentials: true,
+  baseURL: config.apiUrl,
   headers: {'Content-Type': 'application/json;charset=UTF-8'}
 })
 
-// Vue.prototype.$http = apiClient
 export default apiClient

a2143423440d1b1a23efa20d16e7ab1d5b8bd89e
Jiahonzheng <jiahonzheng@gmail.com>
2017-09-14 22:51:15 +0800

    待完善

--- src/pages/UnfinishedTasks.vue
+++ src/pages/UnfinishedTasks.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="text-center">
-    <v-card>
-      <v-card-text>Slient is Gold ?</v-card-text>
-    </v-card>
+    <v-container fluid style="min-height: 0;" grid-list-lg>
+      <v-layout column wrap>
+          <v-flex xs12 v-for="task in tasks" :key="task.ID">
+            <v-card>
+              <v-card-title primary-title>{{task.FROM}}</v-card-title>
+              <v-card-text>{{task.CONTENT}}</v-card-text>
+            </v-card>
+          </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
+  import apiClient from '../modules/apiClient'
+  import {mapState} from 'vuex'
   export default {
     data () {
-      return {}
+      return {
+        tasks: ['TEST']
+      }
     },
-    mounted () {
+    computed: {
+      ...mapState(['userName'])
+    },
+    methods: {
+      checkUnfinishedTasks () {
+        let self = this
+        apiClient.post('/receive', {userName: this.userName, status: 0}).then(function ({data}) {
+          self.tasks = data
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
+    },
+    created () {
+      this.checkUnfinishedTasks()
     }
   }
 </script>

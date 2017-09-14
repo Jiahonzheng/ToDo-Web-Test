f73566821af072f0ecb4aace06c00a390bdf8940
Jiahonzheng <jiahonzheng@gmail.com>
2017-09-14 22:52:31 +0800

    完善状态管理

--- src/store/store.js
+++ src/store/store.js
@@ -17,9 +17,8 @@ const store = new Vuex.Store({
     config: config
   },
   mutations: {
-    setAuth (state, userName) {
-      state.userName = userName
-      global.helper.localStorageManager.set('userName', userName)
+    setUserName (state, data) {
+      state.userName = data
     },
     setMenu (state, data) {
       state.menu = data
@@ -27,14 +26,22 @@ const store = new Vuex.Store({
     setPageTitle (state, data) {
       state.pageTitle = data
     },
-    showMessage (state, type, body) {
+    setMessage (state, type, body) {
       state.message = {type, body}
     }
   },
   actions: {
+    setAuth ({commit}, userName) {
+      commit('setUserName', userName)
+      global.helper.localStorageManager.set('userName', userName)
+    },
     checkAuth ({commit}) {
       let data = global.helper.localStorageManager.get('userName')
-      commit('setAuth', data)
+      commit('setUserName', data)
+    },
+    clearAuth ({commit}) {
+      global.helper.localStorageManager.clear()
+      commit('setUserName', '')
     },
     checkPageTitle ({commit, state}, path) {
       for (let i in state.menu) {

a4e43e767a1f6819fdfb6eaf447d57af53169793
Jiahonzheng <jiahonzheng@gmail.com>
2017-09-23 23:25:58 +0800

    功能基本完善，除DDL颜色提醒功能

--- src/pages/AllTasks.vue
+++ src/pages/AllTasks.vue
@@ -1,17 +1,85 @@
 <template>
-  <div class="text-center">
-    <v-card>
-      <v-card-text>Slient is Gold ?</v-card-text>
-    </v-card>
-  </div>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-list two-line v-if="!!tasks.length">
+        <template v-for="(task, index) in tasks">
+          <v-list-tile :key="index" ripple>
+
+            <v-list-tile-content @click="showDetailsFragment(task.ID, index)">
+              <v-list-tile-title>来自： {{task.FROM}} <v-spacer></v-spacer> 送至： {{task.TO}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{task.CONTENT}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-list-tile-action-text>{{task.END}}</v-list-tile-action-text>
+            </v-list-tile-action>
+
+          </v-list-tile>
+          <v-divider v-if="index + 1 < tasks.length"></v-divider>
+        </template>
+      </v-list>
+    </v-flex>
+
+    <v-snackbar :timeout="3000" :right="true" v-model="toastMessage.show">{{toastMessage.body}}</v-snackbar>
+
+  </v-layout>
 </template>
 
 <script>
+  import apiClient from '../modules/apiClient'
   export default {
     data () {
-      return {}
+      return {
+        tasksList: [],
+        toastMessage: {
+          body: null,
+          show: false
+        }
+      }
     },
-    mounted () {
+    computed: {
+      tasks: {
+        get: function () {
+          return this.tasksList
+        },
+        set: function (newValue) {
+          this.tasksList = newValue
+        }
+      }
+    },
+    methods: {
+      showDetailsFragment (id, index) {
+        this.$store.dispatch('setTempTask', this.tasks[index])
+        this.$router.push({
+          path: this.$route.path + '/' + id
+        })
+      },
+      initScrollPosition () {
+        window.scrollTo(0, 0)
+      },
+      checkAllTasks () {
+        let self = this
+        apiClient.post('/receive', {userName: this.userName, status: 2}).then(function ({data}) {
+          self.tasks = []
+          for (var i = 0; i < data.length; i++) {
+            data[i].BEGIN = global.helper.dateArithmetic.toLocal(data[i].BEGIN)
+            data[i].END = global.helper.dateArithmetic.toLocal(data[i].END)
+            self.tasksList.push(data[i])
+          }
+          if (self.tasks.length === 0) {
+            self.toastMessage.body = '暂无任务'
+            self.toastMessage.show = true
+          }
+        }).catch(function (error) {
+          self.toastMessage.body = '请与管理员联系'
+          self.toastMessage.show = true
+          console.log(error)
+        })
+        this.initScrollPosition()
+      }
+    },
+    created () {
+      this.checkAllTasks()
     }
   }
 </script>
--- src/pages/UnfinishedTasks.vue
+++ src/pages/UnfinishedTasks.vue
@@ -3,55 +3,43 @@
     <v-flex xs12 sm6 offset-sm3>
       <v-list two-line v-if="!!tasks.length">
         <template v-for="(task, index) in tasks">
-          <v-list-tile :key="index" avatar ripple @click="">
+          <v-list-tile :key="index" ripple>
 
-            <v-list-tile-content>
-              <v-list-tile-title>{{task.FROM}}</v-list-tile-title>
+            <v-list-tile-content @click="showDetailsFragment(task.ID, index)">
+              <v-list-tile-title>来自： {{task.FROM}}</v-list-tile-title>
               <v-list-tile-sub-title>{{task.CONTENT}}</v-list-tile-sub-title>
             </v-list-tile-content>
 
             <v-list-tile-action>
               <v-list-tile-action-text>{{task.END}}</v-list-tile-action-text>
-
               <span>
-                <v-btn icon fab>
+                <v-btn icon fab @click.native.stop="achieveButtonPressed(task.ID)">
                   <v-icon>check</v-icon>
                 </v-btn>
               </span>
-
             </v-list-tile-action>
+
           </v-list-tile>
           <v-divider v-if="index + 1 < tasks.length"></v-divider>
         </template>
       </v-list>
-
     </v-flex>
+
+    <v-snackbar :timeout="3000" :right="true" v-model="toastMessage.show">{{toastMessage.body}}</v-snackbar>
+
+    <v-dialog v-model="dialogMessage.show">
+      <v-card>
+        <v-card-title class="headline">{{dialogMessage.body}}</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click.native="dialogMessage.show = false">取消</v-btn>
+          <v-btn flat @click.native="achieveUnfinishedTask">确定</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-layout>
 </template>
-
-<!--
-<template>
-  <div class="text-center">
-    <v-container fluid style="min-height: 0;" grid-list-lg>
-      <v-layout column wrap>
-        <template v-if="!!tasks.length">
-          <v-flex xs12 v-for="task in tasks" :key="task.ID">
-            <v-card>
-              <v-card-title secondary-title>
-                <p class="headline">{{task.FROM}}</p>
-                <p class="headline">{{task.END}}</p>
-              </v-card-title>
-              <v-card-text>
-                <span>{{task.CONTENT}}</span>
-              </v-card-text>
-            </v-card>
-          </v-flex>
-        </template>
-      </v-layout>
-    </v-container>
-  </div>
-</template>
--->
 
 <script>
   import apiClient from '../modules/apiClient'
@@ -59,25 +47,81 @@
   export default {
     data () {
       return {
-        tasks: []
+        tasksList: [],
+        toastMessage: {
+          body: null,
+          show: false
+        },
+        dialogMessage: {
+          body: null,
+          show: false
+        },
+        tempID: null
       }
     },
     computed: {
-      ...mapState(['userName'])
+      ...mapState(['userName', 'scrollPosition']),
+      tasks: {
+        get: function () {
+          return this.tasksList
+        },
+        set: function (newValue) {
+          this.tasksList = newValue
+        }
+      }
     },
     methods: {
       checkUnfinishedTasks () {
         let self = this
         apiClient.post('/receive', {userName: this.userName, status: 0}).then(function ({data}) {
-          // self.tasks = data
+          self.tasks = []
           for (var i = 0; i < data.length; i++) {
             data[i].BEGIN = global.helper.dateArithmetic.toLocal(data[i].BEGIN)
             data[i].END = global.helper.dateArithmetic.toLocal(data[i].END)
-            self.tasks.push(data[i])
+            self.tasksList.push(data[i])
+          }
+          if (self.tasks.length === 0) {
+            self.toastMessage.body = '暂无未完成任务'
+            self.toastMessage.show = true
           }
         }).catch(function (error) {
+          self.toastMessage.body = '请与管理员联系'
+          self.toastMessage.show = true
           console.log(error)
         })
+        this.initScrollPosition()
+      },
+      showDetailsFragment (id, index) {
+        this.$store.dispatch('setTempTask', this.tasks[index])
+        this.$router.push({
+          path: this.$route.path + '/' + id
+        })
+      },
+      initScrollPosition () {
+        window.scrollTo(0, 0)
+      },
+      achieveButtonPressed (id) {
+        this.tempID = id
+        this.dialogMessage.show = true
+        this.dialogMessage.body = '确定完成此项任务？'
+      },
+      achieveUnfinishedTask () {
+        let self = this
+        apiClient.post('/achieve', {ID: this.tempID}).then(({data}) => {
+          if (data.status === 1) {
+            self.toastMessage.body = '成功完成任务'
+            self.toastMessage.show = true
+            self.checkUnfinishedTasks()
+          } else {
+            self.toastMessage.body = '请与管理员联系'
+            self.toastMessage.show = true
+          }
+        }).catch((error) => {
+          self.toastMessage.body = '请与管理员联系'
+          self.toastMessage.show = true
+          console.log(error)
+        })
+        this.dialogMessage.show = false
       }
     },
     created () {
